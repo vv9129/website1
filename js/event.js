ac7103bdc6b831032880f2a5938938f6b0c8512a
vv9129 <venicencm11@gmail.com>
2025-03-03 18:20:30 +0000

    Mon, Mar 3, 2025, 10:20 AM -08:00

--- js/event.js
+++ js/event.js
@@ -1,5 +1,9 @@
 const element1 = document.getElementById('element1')
 
 element1.addEventListener('click', ()=>{
-    element1.innerHTML = ""
+    element1.innerHTML = "I've been clicked! <br> try double clicking me."
+})
+
+element1.addEventListener('dblclick', ()=>{
+    element1.innerHTML = "
 })
