cc06b13fd7081e9e71576660154c60a1c26c78db
vv9129 <venicencm11@gmail.com>
2025-03-11 17:18:18 +0000

    Tue, Mar 11, 2025, 10:18 AM -07:00

--- js/sound.js
+++ js/sound.js
@@ -4,8 +4,9 @@ sounds.forEach(sound => {
     const btn = document.createElement('button')
     btn.classList.add('btn')
     btn.innerHTML = sound
-
-    btn.addEventListener
+    btn.addEventListener('click', () => {
+        document.getElementById(sound).play()
+    })
 
     document.getElementById('buttons').appendChild(btn)
 })
