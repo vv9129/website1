543595a51b4153daa40469c66e426b9ebe4e2601
vv9129 <venicencm11@gmail.com>
2025-03-11 06:19:26 +0000

    Mon, Mar 10, 2025, 11:19 PM -07:00

--- js/interactive.js
+++ js/interactive.js
@@ -1,7 +1,9 @@
 const box = document.querySelector('.box')
 
-const colors = ["lightcoral", "lightsalmon", "lemonchiffon", ]
+const colors = ['lightcoral', 'lightsalmon', 'lemonchiffon', 'lightgreen', 'paleturquoise', 'plum']
+let index = 0
 
 box.addEventListener('click', ()=> {
-    box.style.backgroundColor = "
+    box.style.backgroundColor = colors[index]
+    index = index + 1
 })
