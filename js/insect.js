f0e0360a4bf9d30b50525fdf7d7137073aff6967
vv9129 <venicencm11@gmail.com>
2025-03-21 17:26:24 +0000

    Fri, Mar 21, 2025, 10:26 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -59,6 +59,13 @@ function createInsect(){
 function catchInsect(){
     increaseScore()
     this.classList.add('caught')
+    setTimeout( () => this.remove(), 2000)
+    addInsects()
+}
+
+function addInsects(){
+    setTimeout(createInsect, 1000)
+    setTimeout(createInsect, 1500)
 }
 
 function increaseScore(){
