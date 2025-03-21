764e38b26dca2ad899b20f1402ccf920bc5e20fa
vv9129 <venicencm11@gmail.com>
2025-03-21 17:03:57 +0000

    Fri, Mar 21, 2025, 10:03 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -50,6 +50,7 @@ function createInsect(){
     insect.innerHTML = `<img src="${selected_insect.src}" alt = "${selected_insect.alt}">`
     const [x,y] = getRandomLocation()
     insect.style.top = `${y}px`
+    insect.stle.left = `${x}px`
     game_container.appendChild(insect)
 }
 
