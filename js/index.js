740c9159c795fbb048bee9e06a7d3ea954abd966
vv9129 <venicencm11@gmail.com>
2025-03-31 12:51:58 -0700

    rat

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const buttons = document.querySelectorAll('.btn')
+const text = document.querySelector('h1')
+
+// buttons.addEventListener('mouseover', ()=>{
+//     buttons.style.color = "beige"
+//     buttons.style.backgroundColor = "olive"
+// })
+
+// buttons.addEventListener('mouseout', ()=>{
+//     buttons.style.color = "rosybrown"
+//     buttons.style.backgroundColor = "oldlace"
+// })
+
+text.addEventListener('mouseover', ()=>{
+    text.style.color= "#de5499"
+
+})
+
+text.addEventListener('mouseout', ()=>{
+    text.style.backgroundColor= "#f2ebe9"
+    text.style.color="#264143"
+
+})
+
---
+++ js/relaxer.js
@@ -0,0 +1,23 @@
+const container = document.getElementById('container')
+const text = document.getElementById('text')
+
+setInterval(breatheAnimation, 10000)
+
+function breatheAnimation() {
+    text.innerHTML = 'breathe in'
+    container.classList.remove('shrink')
+    container.classList.add('grow')
+
+    setTimeout(() => {
+        text.innerHTML = 'hold'
+    }, 4000)
+
+    setTimeout(() => {
+        text.innerHTML = 'breathe out'
+        container.classList.remove('grow')
+        container.classList.add('shrink')
+    }, 6000)
+}
+
+breatheAnimation()
+breatheAnimation()
