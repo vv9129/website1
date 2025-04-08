a9f444b3384e4ecba698216eaf0dc6036b6b4f49
vv9129 <venicencm11@gmail.com>
2025-04-08 16:34:28 +0000

    Tue, Apr 8, 2025, 9:34 AM -07:00

--- js/ball.js
+++ js/ball.js
@@ -1,4 +1,12 @@
 const ball = document.createElement('div')
+document.body.appendChild(ball)
+const LPaddle = document.createElement('div')
+document.body.appendChild(LPaddle)
+let LPaddleWidth = 20
+let LPaddleHeight = 100
+let LPaddleSpeed = 20
+
+
 const ballRadius = 50
 const windowHeight = window.innerHeight
 const windowWidth = window.innerWidth
@@ -6,8 +14,9 @@ let ballXPosition = windowWidth / 2 - ballRadius
 let ballYPosition = windowHeight / 2 - ballRadius
 let ballSpeed = 5
 let ballXDirection = 1
-let ballYPosition = 1
+let ballYDirection = 1
 
+let LPaddleYPosition = windowHeight / 2 - LPaddleHeight / 2
 
 createBall()
 setInterval(moveBall, 10)
@@ -23,11 +32,11 @@ function moveBall(){
     if (ballXPosition < 0 || ballXPosition > windowWidth - 2 * ballRadius) {
         ballXDirection = ballXDirection * -1
     }
+    //if (ball.style.top <= LPaddle.style.top && ball.style.bottom >= LPaddle.style.bottom && )
 }
 
 
 function createBall(){
-    document.body.appendChild(ball)
     ball.style.height = `${2*ballRadius}px`
     ball.style.width = `${2*ballRadius}px`
     ball.style.borderRadius = "50%"
@@ -37,3 +46,32 @@ function createBall(){
     ball.style.left = `${windowWidth/2 - ballRadius}px`
 }
 
+createLPaddle()
+function createLPaddle() {
+    LPaddle.style.height = `${LPaddleHeight}px`
+    LPaddle.style.width = `${LPaddleWidth}px`
+    LPaddle.style.backgroundColor = 'blue'
+    LPaddle.style.position = 'absolute'
+    LPaddle.style.left = "50px"
+    LPaddle.style.top = `${LPaddleYPosition}px`
+}
+
+document.addEventListener('keyup', (event)=> {
+    if (event.key == 'w') {
+        if (LPaddleYPosition <= 0) {
+            LPaddleYPosition = 0
+        }
+        else {
+            LPaddleYPosition = LPaddleYPosition - LPaddleSpeed
+        }
+    }
+    if (event.key == 's') {
+        if (LPaddleYPosition >= windowHeight - LPaddleHeight) {
+            LPaddleYPosition = windowHeight - LPaddleHeight
+        }
+        else {
+            LPaddleYPosition = LPaddleYPosition + LPaddleSpeed
+        }
+    }
+    LPaddle.style.top = `${LPaddleYPosition}`
+})
